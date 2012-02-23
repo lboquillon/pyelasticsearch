2523fe13403cc2598a81c9efc77e0e16eb43efa1
Leonardo Boquillon <lboquillon@gmail.com>
2012-02-23 16:03:19 -0430

    Added RangeQuery

--- pyelasticsearch/query.py
+++ pyelasticsearch/query.py
@@ -13,9 +13,25 @@ class Query(object):
     def get_object(self):
         return self._query_object
 
+class RangeQuery(Query):
+
+    def __init__(self, field, from_, to_, include_lower = False, include_upper = False):
+        super(RangeQuery, self).__init__()
+        self.__field = field
+        self.__from = from_
+        self.__to = to_
+        self.__in_lo = include_lower
+        self.__in_up = include_upper
+
+    def build_query(self):
+
+        self._query_object = {'range' : {}}
+        self._query_object['range'][self.__field] = {'from' : self.__from, 'to' : self.__to, 'include_lower' : self.__in_lo, 'include_upper' : self.__in_up}
+
 class TermQuery(Query):
 
     def __init__(self, term, value):
+        super(TermQuery, self).__init__()
         self.__term = term
         self.__value = value
 
@@ -25,6 +41,7 @@ class TermQuery(Query):
 class TermsQuery(Query):
 
     def __init__(self, term, list_value):
+        super(TermsQuery, self).__init__()
         self.__term = term
         self.__list_value = list_value
 
